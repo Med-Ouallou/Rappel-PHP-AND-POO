20cbf4ab499b02e4c2307cfac5ab4efce8882f90
Med-Ouallou <mouallou279@gmail.com>
2025-09-21 14:37:30 +0100

    Composer

---
+++ Composer/blog-tools/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'solicode/cli-tools',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'cb5abf915982c81254bad80b1cea542c98b7bbcd',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'solicode/cli-tools' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'cb5abf915982c81254bad80b1cea542c98b7bbcd',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
